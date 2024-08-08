7a874dcdba2aa541d6005fecabd30c82c4c4f244
_Coder_Naren <narendrajalamkar8399@gmail.com>
2024-08-08 14:06:32 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintingString
+{
+    internal class Program
+    {
+        //Printing String as it is...
+        static void Main(string[] args)
+        {
+            Console.Write("Enter String to be printed :");
+            string name = Console.ReadLine();
+            Console.WriteLine("The string you entered is :" + name);
+            Console.ReadKey();
+        }
+    }
+}
